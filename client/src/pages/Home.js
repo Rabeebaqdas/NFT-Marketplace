034226b5dbe56229dd3a32f45305f9b9bd0cbc09
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2022-09-11 01:30:32 +0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,80 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import Moralis from 'moralis-v1'
-import NFTBox from '../components/NFTBox'
-import { useMoralis } from 'react-moralis'
-
+import React from 'react'
+import nft_gif from "../imgs/nft-gif.gif";
+import News from '../components/NewsHomePage';
+import TrendingCards from '../components/TrendingCards';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const [isConnected, setIsConnected] = useState()
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  
-  const {isWeb3Enabled,account} = useMoralis()
-
-  useEffect(() => {
-    setIsConnected(Moralis.start({ serverUrl: process.env.REACT_APP_SERVER_URL, appId: process.env.REACT_APP_APP_ID }))
-  }, [])
-
-  useEffect(() => {
-
-    const get = async () => {
-
-      try {
-        if (isConnected) {
-          const query = await new Moralis.Query("ActiveItem").ascending("tokenId").notEqualTo("seller",account).find()
-          setData(query)
-          console.log(query)
-       
-          setLoading(false)
-        }
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    get()
-  }, [isConnected,account])
-
-
-
   return (
-    <div className='container mx-auto'>
-      <h1 className='py-4 px-4 font-bold text-2xl'>Recently Listed</h1>
-      <div className='flex flex-wrap'>
-      { data.length !==0 ? (
-         
-      isWeb3Enabled ? 
-      (
-
-      loading ? (<div>Loading......</div>) :
-        ( 
-          data.map((nft) => {
-
-            const { price, nftAddress, tokenId, marketplaceAddress, seller } = nft.attributes
-            return (
-              <div className='m-10'>
-              <NFTBox 
-              price = {price}
-              nftAddress = {nftAddress}
-              tokenId = {tokenId}
-              marketplaceAddress = {marketplaceAddress}
-              seller = {seller}
-              key = {`${nftAddress} ${tokenId}`}
-              />
-              </div>
-            )
-
-          })
-        )
-        ) 
-        :
-        <div>Web3 Currently Not Enabled</div>
-      ) :  "No NFT Avalaible"
-     
-      }
-</div>
-    </div>
+    <>
+    <div className="shadow-none w-full h-80 bg-gradient-to-b from-blue-200 to-transparent flex justify-around flex-wrap items-center">
+        <div className="h-full lg:w-2/3 lg:px-16 md:w-3/5 md:px-8 sm:px-4 sm:w-3/5 xs:w-2/5 flex items-center whitespace-pre-line">
+          <p className="font-bold break-words whitespace-pre-line lg:text-5xl md:text-3xl sm:text-3xl ">
+            Discover, Create and Sell{" "}
+            <p className="text-red-500">Wonderful NFTs</p>
+            <p className="xl:text-xl lg:text-lg md:pt-8 md:w-auto break-words font-medium md:text-lg sm:text-base xs:text-xs xs:w-2/3 xs:pt-2">
+              {" "}
+              We Provide You With A Plethora Of Thrilling NFT Art
+            </p>
+            <Link to="/explore">
+            <button className="py-0.5 px-1 md:text-lg font-medium rounded-md ring ring-2 ring-red-500 bg-red-500 text-gray-100 hover:bg-red-600 xs:ml-4 xs:mt-4 xs:text-sm sm:ml-24 sm:text-base md:ml-28 lg:ml-48 lg:text-lg lg:py-1 lg:px-2">
+              Explore
+            </button>
+            </Link>
+          </p>
+        </div>
+        <div className="h-full lg:w-1/3 lg:px-16 md:w-2/5 md:px-8 sm:w-2/5 xs:w-2/5 flex items-center">
+          <img
+            className="rounded-xl shrink-1 lg:h-64 lg:w-64 md:h-64 md:w-64 sm:h-64 sm:w-64"
+            src={nft_gif}
+          />
+        </div>
+      </div>
+      <News/>
+      <TrendingCards/>
+    </>
   )
 }
 
 export default Home
-
