138b4c3c7760da8d375f5766b147fe0e812656bd
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2022-09-11 01:34:27 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Home from "./pages/Home";
 import MyNFT from './pages/MyNFT';
-import SellNft from "./pages/ListNft";
 import NotListedNFT from './pages/NotListedNFT';
 import MintNFT from './pages/MintNFT';
 import BuyTokens from './pages/BuyTokens';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import { NotListedDetails } from './pages/NotListedDetails';
+import Explore from './pages/Explore';
+import ListedDetails from './pages/ListedDetails';
+import NftDetailsBuy from './pages/NftDetailsBuy';
 
 function App() {
   return (
@@ -15,10 +17,13 @@ function App() {
         <BrowserRouter>
         <Navbar />
        <Routes>
-         <Route exact path='/' element={<Home />} />         
-         <Route path='/listnft' element={<SellNft />} />
+          <Route exact path='/' element={<Home />} />          
+         <Route exact path='/explore' element={<Explore />} /> 
+         <Route exact path='/explore/:id' element={<NftDetailsBuy />} />          
          <Route path='/mynft' element={<MyNFT />} />
-         <Route path='/mintednft' element={<NotListedNFT />} />
+         <Route path='/mynft/:id' element={<ListedDetails />} />
+         <Route path='/notlistednft' element={<NotListedNFT />} />
+         <Route path='/notlistednft/:id' element={<NotListedDetails />} />
          <Route path='/mintnft' element={<MintNFT />} />
          <Route path='/buytokens' element={<BuyTokens />} />
 
